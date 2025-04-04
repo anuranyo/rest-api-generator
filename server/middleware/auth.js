3754aed1a8c455b55e23a8a9a442f9020850d7ff
NureTymofiienkoSnizhana <snizhana.tymofiienko@nure.ua>
2025-04-04 12:24:23 +0300

    /routes: finished authRoutes

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,18 +1,19 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-    const token = req.header('Authorization')?.split(' ')[1];
+    const token = req.header('Authorization')?.replace('Bearer ', '');
 
     if (!token) {
-        return res.status(401).json({ mesagge: 'Відсутній токен авторизації' })
+        return res.status(401).json({ message: 'Відсутній токен авторизації' })
     }
 
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET || 'your_jwt_secret')
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
         req.user = decoded;
         next();
     } catch (err) {
-        res.status(401).json({ mesagge: 'Токен недійсний' })
+        console.error('Помилка при верифікації токена:', err.message);
+        res.status(401).json({ message: 'Токен недійсний' });
     }
 };
--- server/server.js
+++ server/server.js
@@ -1,34 +1,53 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-const authRoutes = require('./routes/authRoutes');
-
 dotenv.config();
+
+const authRoutes = require('./routes/authRoutes');
+const projectRoutes = require('./routes/projectRoutes');
+const schemaRoutes = require('./routes/schemaRoutes');
+const generatorRoutes = require('./routes/generatorRoutes');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+
 app.use('/api/auth', authRoutes);
+//app.use('/api/projects', projectRoutes);
+//app.use('/api/schemas', schemaRoutes);
+//app.use('/api/generator', generatorRoutes);
 
 app.get('/', (req, res) =>{
-   res.send('API сервер працює');  
+   res.send('API Generator Server is running');  
 });
 
-mongoose.connect(process.env.MONGO_URI, {
-    dbName: 'RestApiGenerator'
-    })
-    .then(() => console.log('MongoDB підключено'))
-    .catch(err => {
-        console.error('Помиилка підключення до MongoDB:', err.mesagge);
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            dbName: 'RestApiGenerator',
+            // useNewUrlParser: true,
+            // useUnifiedTopology: true,
+            // useCreateIndex: true,
+            // useFindAndModify: false
+        })
+        .then(() => console.log('MongoDB connected successfully!'))
+        .catch(err => {
+            console.error('Failed to connect to MongoDB:', err.message);
+            console.error('Details:', err);
+        });
+    } catch (err) {
+        console.error('Failed to connect to MongoDB:', err.message);
         process.exit(1);
-    });
+    }
+};
 
-mongoose.connection.on('connected', () => {
-    console.log('Зєднано з базою даних:', mongoose.connection.name);
-});
+connectDB();
 
 const PORT = process.env.PORT || 8000;
+
 app.listen(PORT, () => {
     console.log(`Сервер запущено на порту ${PORT}`)
 });
